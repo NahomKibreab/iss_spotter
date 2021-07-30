d02a2776fbdd4adb174e4a4d0820cbb8b3d4eb27
Nahom Mehanzel <nknahom@gmail.com>
2021-07-30 05:49:48 +0000

    code refactored using promises

--- index.js
+++ index.js
@@ -14,9 +14,7 @@ const { nextISSTimesForMyLocation } = require("./iss");
 const printPassTimes = function(passTimes) {
   for (const pass of passTimes) {
     const datetime = new Date(0);
-    console.log("Before date:", datetime);
     datetime.setUTCSeconds(pass.risetime);
-    console.log(datetime);
     const duration = pass.duration;
     console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
